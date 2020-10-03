d82a7860a44d286539474a75d2317ffb73a5e7f6
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-03 18:54:35 +0530

    Update clone.sh

--- .github/clone.sh
+++ .github/clone.sh
@@ -43,10 +43,10 @@ FILE=/app/bin/
 if [ -d "$FILE" ] ; then
     echo "$FILE directory exists already."
 else
-    mkdir bin
+    mkdir /app/bin/
     # downloading bins
-    wget -O bin/megadown https://raw.githubusercontent.com/adekmaulana/megadown/master/megadown
-    wget -O bin/cmrudl https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py
+    wget -O /app/bin/megadown https://raw.githubusercontent.com/adekmaulana/megadown/master/megadown
+    wget -O /app/bin/cmrudl https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py
     # changing bins permissions
     chmod 755 bin/megadown
     chmod 755 bin/cmrudl 
